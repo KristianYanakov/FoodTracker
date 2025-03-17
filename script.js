11496526bbb4e0f9ee2407f73a9b62d61b46be5e
KristianYanakov <christianyanakov@gmail.com>
2025-03-16 20:28:03 -0700

    implemented a circle tracker

--- script.js
+++ script.js
@@ -14,10 +14,10 @@ class Entry{
     }
 }
 
-
 let entries = []
-//entries.push(new Entry('Burger', 125, 400, 24, 45, 32))
-//entries[0].displayInfo()
+let dailyMacros = {calories: 3000, protein: 180, carbs: 400, fat: 100}
+let macros = {calories: 0, protein: 0, carbs: 0, fat: 0}
+let currProcent = 0
 
 var addFood = function(){
     let foodName = $("#foodname").val()
@@ -39,9 +39,31 @@ var addFood = function(){
 
     
     entries.push(new Entry(foodName, quantity, calories, protein, carbs, fat))
+
     //check if marked as breakfast lunch or dinner and add a class to the <tr> with background color corresponding - lighgreen etc
-    //$(".foodLog").append("<tr><td>" + foodName + "</td> <td>" + quantity + "</td> <td>" + calories + "</td> <td>" + protein + "</td> <td>" + carbs + "</td> <td>" + fat + "</td></tr>")
+    
     $(".foodLog tbody").append("<tr><th scope = 'row'>"+entries.length+"</th> <td>"+ foodName +"</td> <td>"+ quantity +"</td> <td>"+ calories +"</td> <td>"+ protein +"</td> <td>"+ carbs +"</td> <td>"+ fat +"</td> </tr>")
+    
+    //UPDATE THE CALORIE CHART
+    macros.calories += Number(calories)
+    macros.protein += Number(protein)
+    macros.carbs += Number(carbs)
+    macros.fat += Number(fat)
+
+    $("#calorieText p").html(macros.calories + "/" + dailyMacros.calories + " kcal");  //macros.calories + "/"+ dailyMacros.calories + "kcal")
+
+    
+    currProcent = (macros.calories / dailyMacros.calories) * 100;
+
+    if (macros.calories <= dailyMacros.calories / 4) {
+        $(".circle").css("background", "conic-gradient(orange " + currProcent + "%, #e0e0e0 0%)");
+    } else if (macros.calories <= dailyMacros.calories / 2) {
+        $(".circle").css("background", "conic-gradient(yellow " + currProcent + "%, #e0e0e0 0%)");
+    } else if (macros.calories <= dailyMacros.calories * (3 / 4)) {
+        $(".circle").css("background", "conic-gradient(blue " + currProcent + "%, #e0e0e0 0%)");
+    } else {
+        $(".circle").css("background", "conic-gradient(green " + currProcent + "%, #e0e0e0 0%)");
+    }
     
 }
 
@@ -57,3 +79,6 @@ function logEntries(){
     }
 }
 //console.log("TEST LOG FOR GITHUB")
+
+//SAVE INTO TXT FILES WITH THE CURR DATE AND RESULTS
+
